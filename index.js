c18687d3127cdd8c1551a3adf8930749450bed17
Jean M Cetoute <jeanmarylerate51@gmail.com>
2019-08-16 20:18:53 -0500

    added the Routes and the config file

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 const express = require('express');
-const passort = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
 
 const app = express();
 
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/gllgle/callback'
-		},
-		(accessToken) => {
-			console.log(accessToken);
-		}
-	)
-);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => console.log('app is running'));
